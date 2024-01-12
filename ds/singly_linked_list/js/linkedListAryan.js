10558bec715feaa982c486225ff796a440778fb5
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-01-12 22:54:04 +0600

    feat: add more methods according to api

--- ds/singly_linked_list/js/linkedListAryan.js
+++ ds/singly_linked_list/js/linkedListAryan.js
@@ -17,20 +17,93 @@ class LinkedList {
 			return cursor.val
 		}
 	}
-	remove(index) {
+	remove(val) {
+		if(this.head) {
+			let cursor = this.head
+			if (cursor.val === val) {
+				const removedNode = cursor.val
+				this.head = cursor.next
+				return removedNode
+			}
+			while(cursor.next !== null) {
+				if (cursor.next.val === val) {
+					const removedNode = cursor.next.val
+					cursor.next = cursor.next.next
+					return removedNode
+				}
+				cursor = cursor.next
+			}
+		} else {
+			throw new Error('Value doesn\'t exit')
+		}
+		throw new Error('Value doesn\'t exit')
+	}
+	indexOf(val) {
+		if(this.head) {
+			let cursor = this.head
+			let pos = 0
+			if (cursor.val === val) {
+				return pos
+			}
+			while(cursor !== null) {
+				if (cursor.val === val) {
+					return pos
+				}
+				cursor = cursor.next
+			}
+		}
+		throw new Error('Value doesn\'t exit')
+	}
+	get(index) {
 		if(this.head && index < this.size && index >= 0) {
 			let cursor = this.head
 			let pos = 0
 			if (index === 0) {
+				return this.head.val
+			} else {
+				while(index !== pos) {
+					cursor = cursor.next
+					pos += 1
+				}
+				return cursor.val
+			}
+		} else {
+			throw new Error('Index does not exist')
+		}
+	}
+	set(index, val) {
+		if(this.head && index < this.size && index >= 0) {
+			let cursor = this.head
+			let pos = 0
+			if (index === 0) {
+				this.head.val = val
+			} else {
+				while(index !== pos) {
+					cursor = cursor.next
+					pos += 1
+				}
+				cursor.val = val
+			}
+		} else {
+			throw new Error('Index does not exist')
+		}
+	}
+	removeAt(index) {
+		if(this.head && index < this.size && index >= 0) {
+			let cursor = this.head
+			let pos = 0
+			if (index === 0) {
+				const removedVal = this.head.val
 				this.head = null
 			} else {
 				while(index - 1 !== pos) {
 					cursor = cursor.next
 					pos += 1
 				}
-				
+				const removedVal = cursor.next.val
 				cursor.next = cursor.next.next
 			}
+			return removedVal
 		} else {
 			throw new Error('Index does not exist')
 		}
@@ -48,6 +121,28 @@ class LinkedList {
 			this.head = node	
 		}
 		this.size += 1
+	}
+	insert(index, val) {
+		if (index < 0) throw new Error('Index out of bounds')
+		const node = new Node()
+		node.val = val
+		let pos = 0
+		if (index === 0) {
+			node.next = this.head
+			this.head = node
+		} else {
+			let cursor = this.head
+			while(cursor.next !== null && index - 1 !== pos) {
+				cursor = cursor.next
+				pos = pos + 1
+			}
+			if (cursor.next === null && index > pos) {
+				throw new Error('Index out of bounds')
+			}
+			node.next = cursor.next
+			cursor.next = node
+		}
+		size += 1
 	}
 	popBack() {
 		if (this.head) {
